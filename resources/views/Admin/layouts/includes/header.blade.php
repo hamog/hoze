74e62754f9276a4c30edb597b91bf625493dd954
hamog <hashemm364@gmail.com>
2024-01-14 14:29:08 +0330

    Modify header

--- resources/views/Admin/layouts/includes/header.blade.php
+++ resources/views/Admin/layouts/includes/header.blade.php
@@ -15,15 +15,44 @@
           <i class="feather feather-x"></i>
         </a>
       </div>
-      <div class="mt-0">
-        <form class="form-inline">
-          <div class="search-element">
-            <input type="search" class="form-control header-search" placeholder="Search…" aria-label="Search" tabindex="1">
-            <button class="btn btn-primary-color" >
-              <i class="feather feather-search"></i>
+      <div class="d-flex order-lg-2 my-auto mr-auto">
+        <a class="nav-link my-auto icon p-0 nav-link-lg d-md-none navsearch" href="#" data-toggle="search">
+          <i class="feather feather-search search-icon header-icon"></i>
+        </a>
+        <div class="dropdown header-fullscreen">
+          <a class="nav-link icon full-screen-link">
+            <i class="feather feather-maximize fullscreen-button fullscreen header-icons"></i>
+            <i class="feather feather-minimize fullscreen-button exit-fullscreen header-icons"></i>
+          </a>
+        </div>
+        <div class="dropdown profile-dropdown">
+          <a href="#" class="nav-link pr-1 pl-0 leading-none" data-toggle="dropdown">
+            <span>
+              <img src="{{asset('../../assets/images/users/16.jpg')}}" alt="img" class="avatar avatar-md bradius">
+            </span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-left dropdown-menu-arrow animated">
+            <a class="dropdown-item d-flex" href="{{route("admin.profile.edit", auth()->user()->id)}}">
+              <i class="feather feather-user ml-3 fs-16 my-auto"></i>
+              <div class="mt-1">پروفایل</div>
+            </a>
+            <a class="dropdown-item d-flex" href="#">
+              <i class="feather feather-mail ml-3 fs-16 my-auto"></i>
+              <div class="mt-1">Messages</div>
+            </a>
+            <button type="button" class="dropdown-item d-flex" data-toggle="modal" data-target="#changePassswordForm">
+              <i class="feather feather-edit-2 ml-3 fs-16 my-auto"></i>
+              <div class="mt-1">تغییر کلمه عبور</div>
             </button>
+            <a class="dropdown-item d-flex" href="{{ route('admin.logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+              <i class="feather feather-power ml-3 fs-16 my-auto"></i>
+              <div class="mt-1">خروج</div>
+            </a>
+            <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   </div>
